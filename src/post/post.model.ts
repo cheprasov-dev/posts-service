487bea861cbc42e7e41b9aafcafd11cbca39a3be
Alexander Cheprasov <cheprasov.dev@gmail.com>
2023-12-25 00:48:45 +0300

    init module post and added post model

---
+++ src/post/post.model.ts
@@ -0,0 +1,30 @@
+import {
+  Model,
+  Table,
+  Column,
+  HasMany,
+  PrimaryKey,
+  AutoIncrement,
+} from 'sequelize-typescript';
+
+import { Comment } from '../comment/comment.model';
+
+@Table
+export class Post extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @Column
+  title: string;
+
+  @Column
+  content: string;
+
+  @HasMany(() => Comment)
+  comments: Comment[];
+
+  @Column
+  createdBy: number;
+}
