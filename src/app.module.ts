f8e1395afeb76a191de934452e0165d8eb74b4d7
Alexander Cheprasov <cheprasov.dev@gmail.com>
2023-12-25 00:52:14 +0300

    init Post, Comment, Database modules and added validationSchema for ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,33 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { DatabaseModule } from './database/database.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     AuthModule,
+    PostModule,
+    CommentModule,
+    DatabaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
       envFilePath: ['.env.local', '.env.development', '.env'],
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        PG_HOST: Joi.string().required(),
+        PG_PORT: Joi.number().required(),
+        PG_USERNAME: Joi.string().required(),
+        PG_PASSWORD: Joi.string().required(),
+        PG_DATABASE: Joi.string().required(),
+      }),
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
